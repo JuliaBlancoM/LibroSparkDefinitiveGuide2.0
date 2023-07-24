3b05d1b55f2412b7ddd60d2769f48b750da4e2c2
Julia Blanco <julia.blanco@bosonit.com>
2023-07-24 09:04:54 +0200

    Change code MnMs to make it work in Local and IntelliJ

--- src/main/scala/org/libro/Chapter2Scala.scala
+++ src/main/scala/org/libro/Chapter2Scala.scala
@@ -3,37 +3,48 @@ package org.libro
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
 
-
+//Ejercicio MnMs
 object Chapter2Scala {
-
   def main(args: Array[String]) {
-
-    val spark: SparkSession = SparkSession.builder().master("local[1]")
-      .appName("PrimerProyectoSpark")
+    val spark = SparkSession
+      .builder
+      .appName("LibroSpark")
+      .master("local[2]")
       .getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    val rdd = spark.sparkContext.parallelize(List(1, 2, 3, 4, 5))
-    val rddCollect = rdd.collect()
-    println("Number of Partitions: " + rdd.getNumPartitions)
-    println("Action: First element: " + rdd.first())
-    println("Action: RDD converted to Array[Int] : ")
-    rddCollect.foreach(println)
-
-
-    import spark.implicits._
-    val df = spark.createDataFrame(List(("A",1),("B",2),("C",3)))
-    val df2 = Seq((1,"Uno"),(2,"Dos"),(3,"Tres")).toDF("Numero","String")
-    df2.show()
-
-    df.withColumn("Test",lit("AAAA")).show()
-
-    val df3 = spark.read.text("src/main/resources/el_quijote.txt")
-    df3.show()
-    //df.write.csv("src/resources/dfPruebas")
-
-
-
-
+    /*if (args.length < 1) {
+      print("Usage: Chapter2Scala <mnm_file_dataset>")
+      sys.exit(1)
+    }*/
+    // Get the M&M data set filename
+    //val mnmFile = args(0)
+    // Read the file into a Spark DataFrame
+    val mnmDF = spark.read.format("csv")
+      .option("header", "true")
+      .option("inferSchema", "true")
+      .load("src/main/resources/mnm_dataset.csv")
+    // Aggregate counts of all colors and groupBy() State and Color
+    // orderBy() in descending order
+    val countMnMDF = mnmDF
+      .select("State", "Color", "Count")
+      .groupBy("State", "Color")
+      .agg(count("Count").alias("Total"))
+      .orderBy(desc("Total"))
+    // Show the resulting aggregations for all the states and colors
+    countMnMDF.show(60)
+    println(s"Total Rows = ${countMnMDF.count()}")
+    println()
+    // Find the aggregate counts for California by filtering
+    val caCountMnMDF = mnmDF
+      .select("State", "Color", "Count")
+      .where(col("State") === "CA")
+      .groupBy("State", "Color")
+      .agg(count("Count").alias("Total"))
+      .orderBy(desc("Total"))
+    // Show the resulting aggregations for California
+    caCountMnMDF.show(10)
   }
-
 }
+
+//val mnmFilePath = "src/main/resources/mnm_dataset.csv"
+
+//MnMcount.main(Array(mnmFilePath))
