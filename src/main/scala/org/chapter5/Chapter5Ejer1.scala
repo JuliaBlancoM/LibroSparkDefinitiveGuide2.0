0b3fa242692f7f762e3327ebab79c2447586da7e
Julia Blanco <julia.blanco@bosonit.com>
2023-07-27 23:53:15 +0200

    Almost finish Chapter 5

---
+++ src/main/scala/org/chapter5/Chapter5Ejer1.scala
@@ -0,0 +1,123 @@
+package org.chapter5
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+object Chapter5Ejer1 {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("Chapter3Ejer2")
+      .master("local[2]")
+      .getOrCreate()
+    import spark.implicits._
+
+    // Create cubed function
+    val cubed = (s: Long) => {
+      s * s * s
+    }
+
+    // Register UDF
+    spark.udf.register("cubed", cubed)
+
+    // Create temporary view
+    spark.range(1, 9).createOrReplaceTempView("udf_test")
+
+    spark.sql("SELECT id, cubed(id) AS id_cubed FROM udf_test").show()
+
+    //Load and save to a PostgreSQL database using the Spark SQL datasource API and JDBC in Scala:
+    /*val jdbcDF1 = spark
+      .read
+      .format("jdbc")
+      .option("url", "jdbc:postgresql:[DBSERVER]")
+      .option("dbtable", "[SCHEMA].[TABLENAME]")
+      .option("user", "USUARIO")
+      .option("password", "PASSWORD")
+      .load()*/
+
+    import org.apache.spark.sql.Row
+    // Create an array dataset
+    val arrayData = Seq(
+      Row(1, List(1, 2, 3)),
+      Row(2, List(2, 3, 4)),
+      Row(3, List(3, 4, 5))
+    )
+
+    // Create schema
+    val arraySchema = new StructType()
+      .add("id", IntegerType)
+      .add("values", ArrayType(IntegerType))
+
+    // Create DataFrame
+    val df = spark.createDataFrame(spark.sparkContext.parallelize(arrayData), arraySchema)
+    df.createOrReplaceTempView("table")
+    df.printSchema()
+    df.show()
+
+    //Explode and Collect
+    spark.sql(
+      """
+    SELECT id, collect_list(value + 1) AS newValues
+      FROM  (SELECT id, explode(values) AS value
+            FROM table) x
+     GROUP BY id
+    """).show()
+
+    // User Defined Function
+    // Create UDF
+    def addOne(values: Seq[Int]): Seq[Int] = {
+      values.map(value => value + 1)
+    }
+
+    // Register UDF
+    val plusOneInt = spark.udf.register("plusOneInt", addOne(_: Seq[Int]): Seq[Int])
+
+    // Query data
+    spark.sql("SELECT id, plusOneInt(values) AS values FROM table").show()
+
+    // Create DataFrame with two rows of two arrays (tempc1, tempc2)
+    val t1 = Array(35, 36, 32, 30, 40, 42, 38)
+    val t2 = Array(31, 32, 34, 55, 56)
+    val tC = Seq(t1, t2).toDF("celsius")
+    tC.createOrReplaceTempView("tC")
+
+    // Show the DataFrame
+    tC.show()
+
+    // Calculate Fahrenheit from Celsius for an array of temperatures
+    spark.sql("""SELECT celsius, transform(celsius, t -> ((t * 9) div 5) + 32) as fahrenheit FROM tC""").show()
+
+    // Filter temperatures > 38C for array of temperatures
+    spark.sql("""SELECT celsius, filter(celsius, t -> t > 38) as high FROM tC""").show()
+
+    // Is there a temperature of 38C in the array of temperatures
+    spark.sql(
+      """
+    SELECT celsius, exists(celsius, t -> t = 38) as threshold
+    FROM tC
+    """).show()
+
+
+    // Calculate average temperature and convert to F
+    //La función reduce me da error: Exception in thread "main":
+    // Undefined function: reduce. This function is neither a built-in/temporary function,
+    // nor a persistent function that is qualified as spark_catalog.
+
+    spark.sql(
+      """
+    SELECT celsius, ((avg(value) * 9) / 5) + 32 as avgFahrenheit
+    FROM (
+        SELECT celsius, explode(celsius) as value
+        FROM tC
+    ) x
+    GROUP BY celsius
+    """).show()
+
+
+
+
+
+
+  }
+
+}
---
+++ src/main/scala/org/chapter5/Chapter5Ejer2Flights.scala
@@ -0,0 +1,132 @@
+package org.chapter5
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.catalyst.expressions.WindowFunctionType.SQL
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
+object Chapter5Ejer2Flights {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder
+      .appName("Chapter3Ejer2")
+      .master("local[2]")
+      .getOrCreate()
+    import spark.implicits._
+
+    val delaysPath = "src/main/resources/departuredelays.csv"
+    val airportsPath = "src/main/resources/airport-codes-na.txt"
+
+    val airports = spark
+      .read
+      .options(
+        Map(
+          "header" -> "true",
+          "inferSchema" -> "true",
+          "sep" -> "\t"))
+      .csv(airportsPath)
+
+    airports.createOrReplaceTempView("airports_na")
+
+    val delays = spark
+      .read
+      .option("header", "true")
+      .csv(delaysPath)
+      .withColumn("delay", expr("CAST(delay as INT) as delay"))
+      .withColumn("distance", expr("CAST(distance as INT) as distance"))
+
+    delays.createOrReplaceTempView("departureDelays")
+
+    val foo = delays
+      .filter(
+        expr(
+          """
+             origin == 'SEA' AND
+             destination == 'SFO' AND
+             date like '01010%' AND delay > 0
+             """))
+
+    foo.createOrReplaceTempView("foo")
+
+    spark.sql("SELECT * FROM airports_na LIMIT 10").show()
+
+    spark.sql("SELECT * FROM departureDelays LIMIT 10").show()
+
+    spark.sql("SELECT * FROM foo").show()
+
+    // Union two tables
+    val bar = delays.union(foo)
+    bar.createOrReplaceTempView("bar")
+    bar.filter(expr("origin == 'SEA' AND destination == 'SFO' AND date LIKE '01010%' AND delay > 0")).show()
+
+    //Lo mismo en sql
+    spark.sql(
+      """
+    SELECT *
+    FROM bar
+    WHERE origin = 'SEA'
+       AND destination = 'SFO'
+       AND date LIKE '01010%'
+       AND delay > 0
+    """).show()
+
+    // Join Departure Delays data (foo) with flight info
+    foo.join(
+      airports.as('air),
+      $"air.IATA" === $"origin"
+    ).select("City", "State", "date", "delay", "distance", "destination").show()
+
+    //Lo mismo en SQL
+      spark.sql(
+        """
+    SELECT a.City, a.State, f.date, f.delay, f.distance, f.destination
+      FROM foo f
+      JOIN airports_na a
+        ON a.IATA = f.origin
+    """).show()
+
+    //Windowing
+    //ESTE EJEMPLO NO LO PUEDO HACER PORQUE NO ME DEJA CREAR TABLAS PERO LO HE PROBADO EN DATABRICKS
+    val departureDelaysWindow = delays
+      .where($"origin".isin("SEA", "SFO", "JFK") &&
+        $"destination".isin("SEA", "SFO", "JFK", "DEN", "ORD", "LAX", "ATL"))
+      .groupBy("origin", "destination")
+      .agg(sum("delay").alias("TotalDelays"))
+
+    departureDelaysWindow.createOrReplaceTempView("departureDelaysWindow")
+
+    val result = spark.table("departureDelaysWindow")
+    result.show()
+
+    spark.sql(
+      """
+    SELECT origin, destination, TotalDelays, rank
+    FROM (
+    SELECT origin, destination, TotalDelays, dense_rank()
+    OVER (PARTITION BY origin ORDER BY TotalDelays DESC) as rank
+    FROM departureDelaysWindow
+    ) t
+    WHERE rank <= 3
+    """).show()
+
+    //Modifications
+
+    val foo2 = foo.withColumn("status", expr("CASE WHEN delay <= 10 THEN 'On-time' ELSE 'Delayed' END"))
+    foo2.show()
+
+    spark.sql("""SELECT *, CASE WHEN delay <= 10 THEN 'On-time' ELSE 'Delayed' END AS status FROM foo""").show()
+
+    val foo3 = foo2.drop("delay")
+    foo3.show()
+
+    val foo4 = foo3.withColumnRenamed("status", "flight_status")
+    foo4.show()
+
+    //Pivoting
+
+    
+
+
+
+  }
+
+}
