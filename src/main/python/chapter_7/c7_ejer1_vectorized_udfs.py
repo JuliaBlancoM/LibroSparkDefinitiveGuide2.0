90ef67953c250fec9be7489418c6883a08ca68fe
Julia Blanco <julia.blanco@bosonit.com>
2023-08-17 22:57:29 +0200

    ejercicios cap 7

---
+++ src/main/python/chapter_7/c7_ejer1_vectorized_udfs.py
@@ -0,0 +1,40 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import *
+
+spark = SparkSession \
+    .builder \
+    .appName("First PySpark Demo") \
+    .master("local[2]") \
+    .getOrCreate()
+
+# view only the Spark SQL–specific Spark configs
+
+spark.sql("SET -v").select("key", "value").show(n=5, truncate=False)
+
+# comprobar si es modificable y modificar una configuración
+print(spark.conf.isModifiable("spark.sql.shuffle.partitions"))
+spark.conf.get("spark.sql.shuffle.partitions")
+'200'
+
+# Step 1: define input sources
+lines = (spark
+.readStream.format("socket")
+.option("host", "localhost")
+.option("port", 9999)
+.load())
+
+# Step 2: transform data
+words = lines.select(split(col("value"), "\\s").alias("word"))
+counts = words.groupBy("word").count()
+print(counts)
+
+# Step 3: Define Output Sink and Output Mode
+
+writer = counts.writeStream.format("console").outputMode("complete")
+
+# Step4: Specify processing details
+checkpointDir = "C:/Users/julia.blanco/Desktop/repositorios/LibroSpark/src/main/resources/checkpoint"
+writer2 = (writer
+.trigger(processingTime="1 second")
+.option("checkpointLocation", checkpointDir))
